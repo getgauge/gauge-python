533dbde3987860013eeda5ffead1512c697cf775
Kashishm <kashishmunjal64@gmail.com>
2016-02-06 13:59:52 +0530

    Adding set up files for `getgauge` pip package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+    name='getgauge',
+    packages=['getgauge', 'getgauge/messages'],
+    version='0.0.1',
+    description='Enables Python support for Gauge',
+    author='Kashish Munjal',
+    author_email='kashishmunjal64@gmail.com',
+    url='https://github.com/kashishm/gauge-python',
+    download_url='https://github.com/kashishm/gauge-python/releases/download/v0.0.1/gauge-python-0.0.1.zip',
+    keywords=['testing', 'gauge', 'gauge-python', 'getgauge', 'automation'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=['protobuf', 'redBaron'],
+)
