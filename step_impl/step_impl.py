282cf6738aa1623e03edb27d008ca951b9d1763e
Kashishm <kashishmunjal64@gmail.com>
2016-02-06 13:58:16 +0530

    Adding step_impl skel file

---
+++ step_impl/step_impl.py
@@ -0,0 +1,38 @@
+from getgauge.python import step, before_scenario, Messages
+
+vowels = ["a", "e", "i", "o", "u"]
+
+
+def number_of_vowels(word):
+    return len(filter(lambda elem: elem in vowels, list(word)))
+
+
+# --------------------------
+# Gauge step implementations
+# --------------------------
+
+@step("The word <word> has <number> vowels.")
+def assert_no_of_vowels_in(word, number):
+    assert str(number) == str(number_of_vowels(word))
+
+
+@step("Vowels in English language are <vowels>.")
+def assert_default_vowels(given_vowels):
+    Messages.write_message("Given vowels are {0}".format(given_vowels))
+    assert given_vowels == "".join(vowels)
+
+
+@step("Almost all words have vowels <table>")
+def assert_words_vowel_count(table):
+    actual = [str(number_of_vowels(word)) for word in table.get_column_values_with_name("Word")]
+    expected = [str(count) for count in table.get_column_values_with_name("Vowel Count")]
+    assert expected == actual
+
+
+# ---------------
+# Execution Hooks
+# ---------------
+
+@before_scenario()
+def before_scenario_hook():
+    assert "".join(vowels) == "aeiou"
