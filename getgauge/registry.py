bdd259bef76b2e3850f696a6c635fbcc1349424d
Kashishm <kashishmunjal64@gmail.com>
2016-03-05 19:04:39 +0530

    Defining methods dynamically in registry using setattr

--- getgauge/registry.py
+++ getgauge/registry.py
@@ -46,68 +46,27 @@ class Registry(object):
     def __init__(self):
         self.__screenshot_provider = _take_screenshot
         self.__steps_map = {}
-        self.__before_step = []
-        self.__after_step = []
-        self.__before_scenario = []
-        self.__after_scenario = []
-        self.__before_spec = []
-        self.__after_spec = []
-        self.__before_suite = []
-        self.__after_suite = []
+        self.hooks = ['before_step', 'after_step', 'before_scenario', 'after_scenario', 'before_spec', 'after_spec',
+                      'before_suite', 'after_suite']
+        for hook in self.hooks:
+            setattr(self, '__{}'.format(hook), [])
+            self.def_hook_methods(hook)
 
-    def before_step(self, tags=None):
-        return _filter_hooks(tags, self.__before_step)
+    def def_hook_methods(self, hook):
+        def get(self, tags=None):
+            return _filter_hooks(tags, getattr(self, '__{}'.format(hook)))
 
-    def after_step(self, tags=None):
-        return _filter_hooks(tags, self.__after_step)
+        def add(self, func, tags=None):
+            getattr(self, '__{}'.format(hook)).append({'tags': tags, 'func': func})
 
-    def before_scenario(self, tags=None):
-        return _filter_hooks(tags, self.__before_scenario)
-
-    def after_scenario(self, tags=None):
-        return _filter_hooks(tags, self.__after_scenario)
-
-    def before_spec(self, tags=None):
-        return _filter_hooks(tags, self.__before_spec)
-
-    def after_spec(self, tags=None):
-        return _filter_hooks(tags, self.__after_spec)
-
-    def before_suite(self):
-        return self.__before_suite
-
-    def after_suite(self):
-        return self.__after_suite
+        setattr(self.__class__, hook, get)
+        setattr(self.__class__, 'add_{}'.format(hook), add)
 
     def screenshot_provider(self):
         return self.__screenshot_provider
 
     def all_steps(self):
         return [value.step_text for value in self.__steps_map.values()]
-
-    def add_before_step(self, func, tags=None):
-        self.__before_step.append({'tags': tags, 'func': func})
-
-    def add_after_step(self, func, tags=None):
-        self.__after_step.append({'tags': tags, 'func': func})
-
-    def add_before_scenario(self, func, tags=None):
-        self.__before_scenario.append({'tags': tags, 'func': func})
-
-    def add_after_scenario(self, func, tags=None):
-        self.__after_scenario.append({'tags': tags, 'func': func})
-
-    def add_before_spec(self, func, tags=None):
-        self.__before_spec.append({'tags': tags, 'func': func})
-
-    def add_after_spec(self, func, tags=None):
-        self.__after_spec.append({'tags': tags, 'func': func})
-
-    def add_before_suite(self, func):
-        self.__before_suite.append(func)
-
-    def add_after_suite(self, func):
-        self.__after_suite.append(func)
 
     def set_screenshot_provider(self, func):
         self.__screenshot_provider = func
@@ -129,14 +88,8 @@ class Registry(object):
 
     def clear(self):
         self.__steps_map = {}
-        self.__before_step = []
-        self.__after_step = []
-        self.__before_scenario = []
-        self.__after_scenario = []
-        self.__before_spec = []
-        self.__after_spec = []
-        self.__before_suite = []
-        self.__after_suite = []
+        for hook in self.hooks:
+            setattr(self, '__{}'.format(hook), [])
 
 
 registry = Registry()
