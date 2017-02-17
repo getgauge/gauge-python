5144594d8b2f9588bc06a75a024a4b1b6aba9ecf
Kashishm <kashishmunjal64@gmail.com>
2017-02-17 19:51:30 +0530

    Bumping version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='getgauge',
     packages=['getgauge', 'getgauge/messages'],
-    version='0.1.8',
+    version='0.2.0',
     description='Enables Python support for Gauge',
     author='Kashish Munjal',
     author_email='kashishmunjal64@gmail.com',
     url='https://github.com/kashishm/gauge-python',
-    download_url='https://github.com/kashishm/gauge-python/archive/v0.1.8.zip',
+    download_url='https://github.com/kashishm/gauge-python/archive/v0.2.0.zip',
     keywords=['testing', 'gauge', 'gauge-python', 'getgauge', 'automation'],
     license='MIT',
     classifiers=[
