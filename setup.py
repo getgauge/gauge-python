c383b7dd1b4b42c3aaa1f3f79c2f96ec70461ae6
Kashishm <kashishmunjal64@gmail.com>
2017-02-18 12:35:54 +0530

    Removing support for puthon 3.1 and 3.2 (No protobuf support)

--- setup.py
+++ setup.py
@@ -16,11 +16,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=['protobuf==3.2.0', 'redBaron', 'colorama'],
 )
