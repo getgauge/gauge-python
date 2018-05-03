8ce6da0eb22bfc8c4226416ee19e38cafa40e46d
Apoorva G A <apoorvag@thoughtworks.com>
2018-05-03 10:26:39 +0530

    Converting json.loads input to string #53

--- start.py
+++ start.py
@@ -46,7 +46,7 @@ def show_error_exit(pythonPluginVersion, getgaugeVersion):
 def get_version():
     proc = Popen(['gauge', '-v', '--machine-readable'], stdout=PIPE, stderr=PIPE)
     out, _ = proc.communicate()
-    data = json.loads(out)
+    data = json.loads(str(out, 'utf-8'))
     for plugin in data['plugins']:
         if plugin['name'] == 'python':
             return plugin['version']
