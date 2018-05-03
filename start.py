38442e1eacdf26bf558ac3dae212cbcdd7616878
Apoorva G A <apoorvag@thoughtworks.com>
2018-05-03 10:39:31 +0530

    Using decode

--- start.py
+++ start.py
@@ -46,7 +46,7 @@ def show_error_exit(pythonPluginVersion, getgaugeVersion):
 def get_version():
     proc = Popen(['gauge', '-v', '--machine-readable'], stdout=PIPE, stderr=PIPE)
     out, _ = proc.communicate()
-    data = json.loads(str(out, 'utf-8'))
+    data = json.loads(str(out.decode()))
     for plugin in data['plugins']:
         if plugin['name'] == 'python':
             return plugin['version']
