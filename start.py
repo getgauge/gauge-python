6d4c0c3d729600cb7bc88f3840b7f64046207fe4
BugDiver <vinaysh@thoughtworks.com>
2018-05-08 16:45:55 +0530

    Fixed version compatibility check. #71

--- start.py
+++ start.py
@@ -1,13 +1,16 @@
+import json
 import logging
 import os
 import platform
 import sys
 import threading
 from concurrent import futures
+from distutils import version
 from os import path
-import pkg_resources 
+from subprocess import Popen, PIPE
+
 import grpc
-import json
+import pkg_resources
 
 from getgauge import connection, processor
 from getgauge import lsp_server
@@ -15,11 +18,10 @@ from getgauge.impl_loader import copy_skel_files
 from getgauge.messages import lsp_pb2_grpc
 from getgauge.static_loader import load_files
 from getgauge.util import get_step_impl_dir
-from distutils import version
-from subprocess import Popen, PIPE
 
 PLUGIN_JSON = 'python.json'
 VERSION = 'version'
+
 
 def main():
     _init_logger()
@@ -27,21 +29,28 @@ def main():
     if sys.argv[1] == "--init":
         copy_skel_files()
     else:
-        python_plugin_version = get_version()
-        getgauge_version = version.LooseVersion(pkg_resources.get_distribution('getgauge').version)
-        if (list(map(int, python_plugin_version.split(".")[0:3])) != getgauge_version.version[0:3]) or ('dev' in getgauge_version.version and 'nightly' not in python_plugin_version) or ('dev' not in getgauge_version.version and 'nightly' in python_plugin_version):
-            show_error_exit(python_plugin_version, getgauge_version)
-        if 'dev' in getgauge_version.version and 'nightly' in python_plugin_version:
-            python_plugin_version.replace("-","")
-            if python_plugin_version.find(str(getgauge_version.version.pop())) == -1:
-                show_error_exit(python_plugin_version, getgauge_version)
-        else:
-            load_implementations()
-            start()
+        assert_versions()
+        load_implementations()
+        start()
 
-def show_error_exit(pythonPluginVersion, getgaugeVersion):
-    logging.fatal('Gauge-python({}) is not compatible with getgauge({}). Please install compatible versions.\n'.format(pythonPluginVersion, getgaugeVersion))
+
+def assert_versions():
+    python_plugin_version = get_version()
+    getgauge_version = version.LooseVersion(pkg_resources.get_distribution('getgauge').version)
+    if (list(map(int, python_plugin_version.split(".")[0:3])) != getgauge_version.version[0:3]) or (
+            'dev' in getgauge_version.version and 'nightly' not in python_plugin_version) or (
+            'dev' not in getgauge_version.version and 'nightly' in python_plugin_version):
+        show_error_exit(python_plugin_version, getgauge_version)
+    if 'dev' in getgauge_version.version and 'nightly' in python_plugin_version:
+        if str(getgauge_version.version.pop()) not in python_plugin_version.replace("-", ""):
+            show_error_exit(python_plugin_version, getgauge_version)
+
+
+def show_error_exit(python_plugin_version, getgauge_version):
+    logging.fatal('Gauge-python({}) is not compatible with getgauge({}). Please install compatible versions.\n'.format(
+        python_plugin_version, getgauge_version))
     exit(1)
+
 
 def get_version():
     proc = Popen(['gauge', '-v', '--machine-readable'], stdout=PIPE, stderr=PIPE)
@@ -51,6 +60,7 @@ def get_version():
         if plugin['name'] == 'python':
             return plugin['version']
     return ''
+
 
 def load_implementations():
     d = get_step_impl_dir()
