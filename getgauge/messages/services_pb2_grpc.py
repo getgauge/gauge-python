b8d643c6f1a8f12d3322826eab785497a5fc0e3e
Haroon Sheikh <haroon.sheikh@sky.uk>
2023-07-25 23:34:33 +0100

    Upgrading pip dependencies to latest
    Signed-off-by: Haroon Sheikh <haroon.sheikh@sky.uk>

--- getgauge/messages/services_pb2_grpc.py
+++ getgauge/messages/services_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import messages_pb2 as messages__pb2
+import getgauge.messages.messages_pb2 as messages__pb2
 
 
 class RunnerStub(object):
