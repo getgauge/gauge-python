b5ea2aa089f23837e5075673c16b37c587254fda
Kunal Vishwasrao <kunal.vishwasrao@gmail.com>
2024-07-04 21:30:30 +0530

    Adding unittest for relative import (#365)
    Signed-off-by: Kunal Vishwasrao <kunal.vishwasrao@gmail.com>

---
+++ tests/test_impl_loader.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+
+from test_relative_import.relative_import_class import Sample
+from getgauge.impl_loader import update_step_resgistry_with_class
+
+
+class ImplLoaderTest(unittest.TestCase):
+    def setUp(self):
+        self.relative_file_path = os.path.join('..', 'test_relative_import', 'relative_import_class.py')
+
+    def test_update_step_resgistry_with_class(self):
+        curr_dir = os.getcwd()
+        os.chdir('tests')
+        method_list = update_step_resgistry_with_class(Sample(), self.relative_file_path)
+        os.chdir(curr_dir)
+        self.assertEqual(2, len(method_list))
+
+    
+if __name__ == '__main__':
+    unittest.main()
