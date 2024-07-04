ec73984665836bbfa010fd7c0f719425737ce7e8
Kunal Vishwasrao <kunal.vishwasrao@gmail.com>
2024-07-05 00:16:03 +0530

    Updating unittest for impl loader (#365)
    Signed-off-by: Kunal Vishwasrao <kunal.vishwasrao@gmail.com>

--- tests/test_impl_loader.py
+++ tests/test_impl_loader.py
@@ -2,7 +2,7 @@ import os
 import unittest
 
 from test_relative_import.relative_import_class import Sample
-from getgauge.impl_loader import update_step_resgistry_with_class
+from getgauge.impl_loader import update_step_registry_with_class
 
 
 class ImplLoaderTest(unittest.TestCase):
@@ -12,9 +12,11 @@ class ImplLoaderTest(unittest.TestCase):
     def test_update_step_resgistry_with_class(self):
         curr_dir = os.getcwd()
         os.chdir('tests')
-        method_list = update_step_resgistry_with_class(Sample(), self.relative_file_path)
+        method_list = update_step_registry_with_class(Sample(), self.relative_file_path)
         os.chdir(curr_dir)
-        self.assertEqual(2, len(method_list))
+        self.assertEqual(["Greet <name> from inside the class", 
+                          "Greet <name> from outside the class"], 
+                          [method.step_text for method in method_list])
 
     
 if __name__ == '__main__':
