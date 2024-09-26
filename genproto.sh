f0493d2d023d723e44eea3c5e0b1055c1922b602
Chad Wilson <chadw@thoughtworks.com>
2024-09-26 21:25:13 +0800

    Re-generate protos from latest (#393)
    Signed-off-by: Chad Wilson <chadw@thoughtworks.com>
    Co-authored-by: Srikanth <srikanth.ddit@gmail.com>

--- genproto.sh
+++ genproto.sh
@@ -6,6 +6,6 @@ fi
 
 ${GAUGE_PYTHON_COMMAND} -m grpc_tools.protoc -I gauge-proto --python_out=getgauge/messages --grpc_python_out=getgauge/messages gauge-proto/*.proto
 git clean -fx
-warning="Please update the import staement \n\t'import spec_pb2 as spec__pb2'\n to \n\t'import getgauge.messages.spec_pb2 as spec__pb2'\nin following files -\n\tmessages_pb2.py\n\tlsp_pb2.py\n\tapi_pb2.py
+warning="Please update the import statement \n\t'import spec_pb2 as spec__pb2'\n to \n\t'import getgauge.messages.spec_pb2 as spec__pb2'\nin following files -\n\tmessages_pb2.py\n\tlsp_pb2.py\n\tapi_pb2.py
 \n\nTHIS IS DONE IN ORDER TO ADDRESS RELATIVE IMPORT ISSUE IN DIFFERENT PYTHON VERSIONS."
 echo $warning
