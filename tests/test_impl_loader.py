5ad27475747eaaa8d3a65608a9601c72d3849e60
Kunal Vishwasrao <36107786+kunalvishwasrao@users.noreply.github.com>
2025-03-29 10:58:49 +0530

    Fix for issue with loading step impls from relative dirs (#430)
    * Updating check for relative paths
    
    Signed-off-by: Kunal Vishwasrao <kunal.vishwasrao@gmail.com>
    
    * Added unit test scenario with '..' in between path
    
    Signed-off-by: Kunal Vishwasrao <kunal.vishwasrao@gmail.com>
    
    * Added proper comment to the relative file path check
    
    Signed-off-by: Kunal Vishwasrao <kunal.vishwasrao@gmail.com>
    
    * Bump version
    
    Signed-off-by: Chad Wilson <chadw@thoughtworks.com>
    
    ---------
    
    Signed-off-by: Kunal Vishwasrao <kunal.vishwasrao@gmail.com>
    Signed-off-by: Chad Wilson <chadw@thoughtworks.com>
    Co-authored-by: Chad Wilson <chadw@thoughtworks.com>

--- tests/test_impl_loader.py
+++ tests/test_impl_loader.py
@@ -7,13 +7,21 @@ from test_relative_import.relative_import_class import Sample
 
 class ImplLoaderTest(unittest.TestCase):
     def setUp(self):
+        self.curr_dir = os.getcwd()
         self.relative_file_path = os.path.join('..', 'test_relative_import', 'relative_import_class.py')
+        self.relative_file_path_one_level_above = os.path.join('tests', '..', 'test_relative_import', 'relative_import_class.py')
 
     def test_update_step_registry_with_class(self):
-        curr_dir = os.getcwd()
         os.chdir('tests')
         method_list = update_step_registry_with_class(Sample(), self.relative_file_path)
-        os.chdir(curr_dir)
+        os.chdir(self.curr_dir)
+        self.assertEqual(["Greet <name> from inside the class", 
+                          "Greet <name> from outside the class"], 
+                          [method.step_text for method in method_list])
+
+    def test_update_step_registry_with_class_one_level_above(self):
+        os.chdir(self.curr_dir)
+        method_list = update_step_registry_with_class(Sample(), self.relative_file_path_one_level_above)
         self.assertEqual(["Greet <name> from inside the class", 
                           "Greet <name> from outside the class"], 
                           [method.step_text for method in method_list])
