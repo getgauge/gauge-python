3e5418f7370d901c6d708865ced67bb59229cfc5
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-05-19 18:31:54 +0800

    Allow GAUGE_PYTHON_COMMAND to contain spaces (#438)
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-IF "%GAUGE_PYTHON_COMMAND%"=="" (SET GAUGE_PYTHON_COMMAND="python")
-%GAUGE_PYTHON_COMMAND% check_and_install_getgauge.py
-%GAUGE_PYTHON_COMMAND% -u start.py %1
+IF "%GAUGE_PYTHON_COMMAND%"=="" (SET GAUGE_PYTHON_COMMAND=python)
+"%GAUGE_PYTHON_COMMAND%" check_and_install_getgauge.py
+"%GAUGE_PYTHON_COMMAND%" -u start.py %1
