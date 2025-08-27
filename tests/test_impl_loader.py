9a063f7a2c968711ee0f8d51a30765395c4ac34e
sschulz92 <bastie92_spam@gmx.de>
2025-08-27 14:12:36 +0200

    Temporary changes
    Signed-off-by: sschulz92 <bastie92_spam@gmx.de>

--- tests/test_impl_loader.py
+++ tests/test_impl_loader.py
@@ -1,30 +1,60 @@
 import os
 import unittest
+from pathlib import Path
 
-from getgauge.impl_loader import update_step_registry_with_class
-from test_relative_import.relative_import_class import Sample
+from getgauge.impl_loader import load_impls
+from getgauge.registry import registry
 
 
 class ImplLoaderTest(unittest.TestCase):
-    def setUp(self):
-        self.curr_dir = os.getcwd()
-        self.relative_file_path = os.path.join('..', 'test_relative_import', 'relative_import_class.py')
-        self.relative_file_path_one_level_above = os.path.join('tests', '..', 'test_relative_import', 'relative_import_class.py')
 
     def test_update_step_registry_with_class(self):
-        os.chdir('tests')
-        method_list = update_step_registry_with_class(Sample(), self.relative_file_path)
-        os.chdir(self.curr_dir)
-        self.assertEqual(["Greet <name> from inside the class", 
-                          "Greet <name> from outside the class"], 
-                          [method.step_text for method in method_list])
+
+        tests_directory = str(Path(__file__).resolve().parent)
+        relative_file_path = os.path.join('..', 'test_relative_import')
+
+        load_impls(
+            step_impl_dirs=[relative_file_path],
+            project_root=tests_directory
+        )
+
+        loaded_steps = registry.get_steps_map()
+
+        self.assertEqual(2, len(loaded_steps))
+
+        step_infos_of_class_instance = loaded_steps["Greet {} from inside the class"]
+
+        self.assertEqual(1, len(step_infos_of_class_instance))
+        self.assertIsNotNone(step_infos_of_class_instance[0].instance)
+
+        self.assertEqual(
+            ["Greet <name> from inside the class", "Greet <name> from outside the class"],
+            registry.steps()
+        )
 
     def test_update_step_registry_with_class_one_level_above(self):
-        os.chdir(self.curr_dir)
-        method_list = update_step_registry_with_class(Sample(), self.relative_file_path_one_level_above)
-        self.assertEqual(["Greet <name> from inside the class", 
-                          "Greet <name> from outside the class"], 
-                          [method.step_text for method in method_list])
+
+        repo_root_directory = str(Path(__file__).resolve().parent.parent)
+        relative_file_path_one_level_above = os.path.join('tests', '..', 'test_relative_import')
+
+        load_impls(
+            step_impl_dirs=[relative_file_path_one_level_above],
+            project_root=repo_root_directory
+        )
+
+        loaded_steps = registry.get_steps_map()
+
+        self.assertEqual(2, len(loaded_steps))
+
+        step_infos_of_class_instance = loaded_steps["Greet {} from inside the class"]
+
+        self.assertEqual(1, len(step_infos_of_class_instance))
+        self.assertIsNotNone(step_infos_of_class_instance[0].instance)
+
+        self.assertEqual(
+            ["Greet <name> from inside the class", "Greet <name> from outside the class"],
+            registry.steps()
+        )
 
 
 if __name__ == '__main__':
